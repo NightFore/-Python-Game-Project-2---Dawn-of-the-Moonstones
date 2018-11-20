aaed8e81d483f38bef1236439c40deece030cd3c
NightFore <38656758+NightFore@users.noreply.github.com>
2018-11-20 22:19:53 +0100

    update

--- [Setup Build]/setup-v-6-0-b.py
+++ [Setup Build]/setup-v-6-0-b.py
@@ -4,7 +4,7 @@ PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
-executables = [cx_Freeze.Executable("Dawn of the Moonstones v6-0b.py")]
+executables = [cx_Freeze.Executable("[Game Project 2] Dawn of the Moonstones v6-0b.py")]
 
 cx_Freeze.setup(
     name="Dawn of the Moonstones",
